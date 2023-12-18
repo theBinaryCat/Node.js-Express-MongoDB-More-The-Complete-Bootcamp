05848adb7e9cdf2a5e1edfc2e3147458952ba720
theBinaryCat <noua.san@gmail.com>
2023-12-18 13:41:41 +0100

    get tours api

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,9 +1,23 @@
 const express = require('express')
-
+const fs = require('fs')
 const app = express()
-app.get('/', (req, res) => {
-    res.status(200).send("Hello, world!")
+
+// app.get('/', (req, res) => {
+//     res.status(200).send("Hello, world!")
+// })
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+app.get('/api/v1/tours', (req, res) => {
+    res
+    .status(200)
+    .json({
+        status: "success",
+        data: {tours}
+    })
 })
+
 const port = 3000
 app.listen(port,() =>{
     console.log('listening on port ${port}')
