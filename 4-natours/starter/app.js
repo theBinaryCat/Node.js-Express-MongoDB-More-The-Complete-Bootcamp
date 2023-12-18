ffc09f7e28e6d7496b69a65187e3286b107061db
theBinaryCat <noua.san@gmail.com>
2023-12-18 13:16:13 +0100

    listening on port

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+app.get('/', (req, res) => {
+    res.status(200).send("Hello, world!")
+})
+const port = 3000
+app.listen(port,() =>{
+    console.log('listening on port ${port}')
+})
