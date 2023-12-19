a511a4dbebd8c657333b98c56d5734811c0c5ae3
theBinaryCat <noua.san@gmail.com>
2023-12-20 00:34:17 +0100

    ad dserver.js file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,51 +5,16 @@ const userRouter = require('./routes/userRoutes')
 const morgan = require('morgan')
 const app = express()
 
-
+//middleware
 //get (console.log) info about request 
 app.use(morgan('dev'))
-//parse request bodes with json 
+//parse request bodies with json 
 app.use(express.json())
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users',userRouter)
-// app.get('/', (req, res) => {
-//     res.status(200).send("Hello, world!")
-// })
-// const tours = JSON.parse(
-//     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-// )
-
-//middleware
-app.use((req, res, next) => {
-    console.log('this is  a middleware')
-    next()
-})
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     next()
 })
 
-
-//user routes
-//const userRouter = express.Router()
-
-//routes
-// userRouter.
-//     route('/').
-//     get(getAllUsers).
-//     post(createUsers)
-
-// tourRouter.
-//     route('/:id').
-//     get(getTout).
-//     patch(updateUsers).
-//     delete(deleteUsers)
-
-// app.use('/api/v1/users', userRouter)
-
-//start the server
-const port = 3000
-app.listen(port,() =>{
-    console.log(`listening on port ${port}`)
-})
+module.exports = app
---
+++ 4-natours/starter/server.js
@@ -0,0 +1,6 @@
+const app = require('./app')
+//start the server
+const port = 3000
+app.listen(port,() =>{
+    console.log(`listening on port ${port}`)
+})
